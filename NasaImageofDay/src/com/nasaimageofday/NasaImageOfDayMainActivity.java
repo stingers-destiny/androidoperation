31f950e52216c54564208931be88f4ff94fa94c7
ammar <contactammar@gmail.com>
2013-08-14 00:09:31 +0200

    1. committing from staged changes

--- NasaImageofDay/src/com/nasaimageofday/NasaImageOfDayMainActivity.java
+++ NasaImageofDay/src/com/nasaimageofday/NasaImageOfDayMainActivity.java
@@ -1,21 +1,45 @@
 package com.nasaimageofday;
 
-import android.app.Activity;
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class NasaImageOfDayMainActivity extends Activity {
+public class NasaImageOfDayMainActivity extends FragmentActivity {
 	private static final String URL = "http://www.nasa.gov/rss/image_of_the_day.rss";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_nasa_image_of_day_main);
-		new UpdateRssAsyncTask().execute(URL);
+		refresh();
+	}
+
+	private void refresh() {
+		if (isInternetAvailable()) {
+			new UpdateRssAsyncTask().execute(URL);
+		} else {
+			DialogFragment fragment = new NoInternetConnectionDialogFragment();
+			fragment.show(getSupportFragmentManager(), "");
+		}
+	}
+
+	public void refreshActivity(View view) {
+		refresh();
+	}
+
+	private boolean isInternetAvailable() {
+		ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+		return networkInfo != null && networkInfo.isConnected();
 	}
 
 	@Override
