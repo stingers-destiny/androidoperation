65f66ac52caa33060ea4f48808d803ac4b9eb7e5
ammar <contactammar@gmail.com>
2013-08-15 00:06:58 +0200

    remove unused imports

--- NasaImageofDay/src/com/nasaimageofday/RssHandler.java
+++ NasaImageofDay/src/com/nasaimageofday/RssHandler.java
@@ -5,7 +5,6 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.text.AttributedCharacterIterator.Attribute;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
