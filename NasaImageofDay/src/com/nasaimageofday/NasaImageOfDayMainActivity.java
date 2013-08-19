522b1d6151922cc33fe6674ee22b0649a25fbfbf
ahassan <ahassan@ahassan-pc.360t.com>
2013-08-19 18:32:52 +0200

    add functionality for #2

--- NasaImageofDay/src/com/nasaimageofday/NasaImageOfDayMainActivity.java
+++ NasaImageofDay/src/com/nasaimageofday/NasaImageOfDayMainActivity.java
@@ -2,12 +2,18 @@ package com.nasaimageofday;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Typeface;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.StyleSpan;
+import android.text.style.URLSpan;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ImageView;
@@ -23,7 +29,18 @@ public class NasaImageOfDayMainActivity extends FragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_nasa_image_of_day_main);
+		setVisibilityForMoreOrLessLink(false);
 		refresh();
+	}
+
+	public void refreshActivity(View view) {
+		refresh();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.nasa_image_of_day_main, menu);
+		return true;
 	}
 
 	private void refresh() {
@@ -35,20 +52,20 @@ public class NasaImageOfDayMainActivity extends FragmentActivity {
 		}
 	}
 
-	public void refreshActivity(View view) {
-		refresh();
-	}
-
 	private boolean isInternetAvailable() {
 		ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 		NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
 		return networkInfo != null && networkInfo.isConnected();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.nasa_image_of_day_main, menu);
-		return true;
+	private void setVisibilityForMoreOrLessLink(boolean b) {
+		SpannableString ss = new SpannableString("Show More");
+		ss.setSpan(new StyleSpan(Typeface.ITALIC), 0, 6, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+		ss.setSpan(new URLSpan("tel:4155551212"), 13, 17, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+		TextView view = (TextView) findViewById(R.id.imageDescriptionMoreOrLess);
+		view.setMovementMethod(LinkMovementMethod.getInstance());
+
 	}
 
 	private class UpdateRssAsyncTask extends
@@ -72,9 +89,9 @@ public class NasaImageOfDayMainActivity extends FragmentActivity {
 			((TextView) findViewById(R.id.title)).setText(title);
 			((TextView) findViewById(R.id.publishDate)).setText(date);
 			((ImageView) findViewById(R.id.imageDisplay)).setImageBitmap(image);
-			((TextView) findViewById(R.id.imageDescription))
-					.setText(description);
-
+			((TextView) findViewById(R.id.imageDescription)).setText(description);
+			String smallDescription = (String)((description == null || description.length() < 20) ? description : description.substring(0, 20));
+			((TextView) findViewById(R.id.imageDescriptionMoreOrLess)).setText(smallDescription);
 		}
 	}
 }
